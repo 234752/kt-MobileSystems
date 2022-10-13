7fe4f38baecda986b60342e677329be572cec5dd
Patryk <patryk.gielcz@gmail.com>
2022-10-13 13:54:35 +0200

    everything works

--- PoG/app/src/main/java/com/pog/MainActivity.kt
+++ PoG/app/src/main/java/com/pog/MainActivity.kt
@@ -2,24 +2,45 @@ package com.pog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import java.lang.Math.sqrt
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var result = findViewById<TextView>("TextViewResult")
+        var result = findViewById<TextView>(R.id.textViewResult)
+        result.text = ""
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val a : Double = findViewById<EditText>(R.id.editTextA).text.toString().toDouble()
+            val b : Double = findViewById<EditText>(R.id.editTextB).text.toString().toDouble()
+            val c : Double = findViewById<EditText>(R.id.editTextC).text.toString().toDouble()
+            result.text = calculateResult(a, b, c)
+        }
     }
 
     fun calculateResult(a : Double, b : Double, c : Double): String {
+
+        if(a == 0.0) {
+            var x = round(-c/b*100)/100
+            return "It's a linear equation, result: \nx = $x"
+        }
+
         var d = b*b - 4*a*c
         if(d > 0) {
-            var x1 = (sqrt(d) - b)/(2*a)
-            var x2 = (sqrt(d) + b)/(2*a)
-            return "Results: x\u2081 = $x1, x\u2082 = $x2"
+            var x1 = round((sqrt(d) - b)/(2*a)*100)/100
+            var x2 = round((sqrt(d) + b)/(2*a)*100)/100
+            return "Results: \nx\u2081 = $x1 \nx\u2082 = $x2"
+        } else if(d == 0.0) {
+            var x0 = round(-b/(2*a)*100)/100
+            return "Result: \nx\u2080 = $x0"
+        } else {
+            return "Discriminant smaller than 0, no results in this case"
         }
-        else return "pog"
     }
 }
