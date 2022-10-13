0be18a77ff6fe72a18c37a518fcaf813c36c6da9
Patryk <patryk.gielcz@gmail.com>
2022-10-13 11:07:37 +0200

    part of result's logic

--- PoG/app/src/main/java/com/pog/MainActivity.kt
+++ PoG/app/src/main/java/com/pog/MainActivity.kt
@@ -2,10 +2,24 @@ package com.pog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.lang.Math.sqrt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var result = findViewById<TextView>("TextViewResult")
+    }
+
+    fun calculateResult(a : Double, b : Double, c : Double): String {
+        var d = b*b - 4*a*c
+        if(d > 0) {
+            var x1 = (sqrt(d) - b)/(2*a)
+            var x2 = (sqrt(d) + b)/(2*a)
+            return "Results: x\u2081 = $x1, x\u2082 = $x2"
+        }
+        else return "pog"
     }
 }
