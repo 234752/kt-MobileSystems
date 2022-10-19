4bab232d8b4c5b98218d7c0cf0a677d57130e0f1
Patryk <patryk.gielcz@gmail.com>
2022-10-19 15:46:07 +0200

    sensor implemented - does not work tho

--- PoG/app/src/main/java/com/pog/DiceViewModel.kt
+++ PoG/app/src/main/java/com/pog/DiceViewModel.kt
@@ -1,15 +1,22 @@
 package com.pog
 
+import android.R.attr.gravity
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-import java.lang.Math.sqrt
-import kotlin.math.round
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
-class DiceViewModel : AppCompatActivity() {
+
+class DiceViewModel : AppCompatActivity(), SensorEventListener {
+
+    var sensorManager : SensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+    var tiltSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dice_view)
@@ -21,10 +28,8 @@ class DiceViewModel : AppCompatActivity() {
         val buttonThree = findViewById<RadioButton>(R.id.three)
         choice.check(buttonOne.id)
 
-        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        val tiltSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
-
-
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        tiltSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
         choice.setOnCheckedChangeListener(
             RadioGroup.OnCheckedChangeListener { group, checkedId ->
@@ -39,5 +44,40 @@ class DiceViewModel : AppCompatActivity() {
 
     private fun getRandomNumber(): Int {
         return Random.nextInt(1, 7);
+    }
+
+    override fun onResume() {
+        super.onResume()
+//        tiltSensor?.also { tilt ->
+//            sensorManager.registerListener(this, tilt, SensorManager.SENSOR_DELAY_NORMAL)
+//        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        // alpha is calculated as t / (t + dT)
+        // with t, the low-pass filter's time-constant
+        // and dT, the event delivery rate
+        val alpha = 0.8.toFloat()
+        val gravityValues = FloatArray(3)
+        gravityValues[0] = alpha * gravityValues[0] + (1 - alpha) * event.values[0]
+        gravityValues[1] = alpha * gravityValues[1] + (1 - alpha) * event.values[1]
+        gravityValues[2] = alpha * gravityValues[2] + (1 - alpha) * event.values[2]
+        val linear_acceleration = FloatArray(3)
+        linear_acceleration[0] = event.values[0] - gravityValues[0]
+        linear_acceleration[1] = event.values[1] - gravityValues[1]
+        linear_acceleration[2] = event.values[2] - gravityValues[2]
+        if(linear_acceleration[0] > 1) {
+            val result = findViewById<TextView>(R.id.textView)
+            result.text ="pog"
+        }
     }
 }
