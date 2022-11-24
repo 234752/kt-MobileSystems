4738d87734c660356048b1e6212466752d4f939a
Patryk <patryk.gielcz@gmail.com>
2022-11-24 18:12:43 +0100

    minor changes

--- PoG/app/src/main/java/com/pog/ListViewModel.kt
+++ PoG/app/src/main/java/com/pog/ListViewModel.kt
@@ -1,23 +1,33 @@
 package com.pog
 
-import android.R.attr.gravity
-import android.app.Activity
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.os.Bundle
-import android.widget.*
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.random.Random
+import java.util.*
 
+class ListViewModel : AppCompatActivity() {
 
-class ListViewModel : Activity() {
-
-
+    private var itemList: ArrayList<ListItem>? = null
+    private lateinit var listView: ListView
+    private lateinit var itemsTable: ItemsTable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_view)
+        title = "YOUR GROCERY LIST"
+        listView = findViewById<View>(R.id.itemListView) as ListView
+        itemList = ArrayList<ListItem>()
+        itemList!!.add(ListItem("it1", 1))
+        itemList!!.add(ListItem("it2", 2))
+        itemList!!.add(ListItem("it3", 3))
+        itemsTable = ItemsTable(itemList!!, applicationContext)
+        listView.adapter = itemsTable
+        listView.onItemClickListener = OnItemClickListener { _, _, position, _ ->
+            val dataModel: ListItem = itemList!![position] as ListItem
+            dataModel.IsBought = !dataModel.IsBought
+            itemsTable.notifyDataSetChanged()
+        }
     }
 
 }
