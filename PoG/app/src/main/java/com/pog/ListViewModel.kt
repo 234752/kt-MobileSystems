ef79cead14e57c1eedf25b124fbb8e123af42836
Patryk <patryk.gielcz@gmail.com>
2022-12-07 23:49:56 +0100

    dialog fixed

--- PoG/app/src/main/java/com/pog/ListViewModel.kt
+++ PoG/app/src/main/java/com/pog/ListViewModel.kt
@@ -3,11 +3,14 @@ package com.pog
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.ListView
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -43,18 +46,34 @@ class ListViewModel : AppCompatActivity() {
             val builder: AlertDialog.Builder = android.app.AlertDialog.Builder(this)
             builder.setTitle("Enter the name of item:")
 
-            builder.setView(R.layout.new_item_popup)
-            val view = builder.create()
 
-            val itemName = view.findViewById<EditText>(R.id.inputName)
-            val itemAmount = view.findViewById<EditText>(R.id.inputAmount)
+            val view = LinearLayout(this)
+            view.orientation = LinearLayout.VERTICAL
+
+
+            val itemName = EditText(this)
+            val itemAmount = EditText(this)
+            val itemUnit = EditText(this)
+            itemName.setRawInputType(InputType.TYPE_CLASS_TEXT)
+            itemAmount.setRawInputType(InputType.TYPE_CLASS_NUMBER)
+            itemUnit.setRawInputType(InputType.TYPE_CLASS_TEXT)
+            view.addView(itemName)
+            view.addView(itemAmount)
+            view.addView(itemUnit)
+
+            itemAmount.setText("1")
+            itemUnit.setText("pieces")
+
+            builder.setView(view)
+
+
 
             builder.setPositiveButton("OK",
                 DialogInterface.OnClickListener { dialog, which ->
                         newItemName = itemName.text.toString()
-                        //newItemAmount = itemAmount.text.toString().toInt()
-                        //itemList!!.add(ListItem("jj", 1))
-                        //itemsTable.notifyDataSetChanged()
+                        newItemAmount = itemAmount.text.toString().toInt()
+                        itemList!!.add(ListItem(newItemName, newItemAmount))
+                        itemsTable.notifyDataSetChanged()
 
                 })
             builder.setNegativeButton("Cancel",
